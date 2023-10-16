a66ba80deec3d1ecd32f23ad5af2fdde3c2d83f9
¨jzvolensky¨ <juro.zvolensky@gmail.com>
2023-10-16 12:42:32 +0000

    someprogress

---
+++ src/inference.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import logging
+import math
+import argparse
+
+from sentinelhub import (
+    BBox,
+    CRS,
+    DataCollection,
+    DownloadFailedException,
+    MimeType,
+    SentinelHubRequest,
+    SHConfig,
+    bbox_to_dimensions,
+)
+
+relative_paths = ["./utils", "./models", "./draft_model"]
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.extend(os.path.join(current_dir, rel_path) for rel_path in relative_paths)
+
+from utils import request_data_sh, ship_detector
+from utils import *
+from models import *
+
+import argparse
+
+args = None
+INSTANCE_ID = None
+CLIENT_ID = None
+CLIENT_SECRET = None
+LAYER_NAME = None
+BBOX = None
+TIME= None
+MAX_CC = None
+THRESHOLD = None
+
+#TODO: FIX THE SENTINELHUB INTEGRATION TO SOMEHOW ACCEPT DYNAMIC IMAGE SIZE BASED ON THE INPUT BBOX
+#TODO: CHECK IF THE BANDS ARE BEING CORRECTLY LOADED AND USED BY THE SHIP DETECTION FUNCTION
+
+def setup_argparse():
+    '''
+    Function to setup the input parameters for the application package
+    --instance_id: Sentinel Hub instance ID
+    --layer: Sentinel Hub layer name
+    --bbox: Bounding box in the format minx,miny,maxx,maxy
+    --time: Time range of the image in the format YYYY-MM-DD/YYYY-MM-DD
+    --width: Width of the output image, in pixels
+    --height: Height of the output image, in pixels
+    --maxcc: Maximum cloud cover percentage
+    '''
+    global args, INSTANCE_ID, CLIENT_ID, CLIENT_SECRET, BBOX, TIME, MAX_CC, THRESHOLD
+
+    args = argparse.ArgumentParser(description='Sentinel Hub Inputs')
+    args.add_argument('--instance_id', type=str, default='your_instance_id', help='Sentinel Hub instance ID')
+    args.add_argument('--client_id', type=str, default='your_client_id', help='Sentinel Hub client ID')
+    args.add_argument('--client_secret', type=str, default='your_client_secret', help='Sentinel Hub client secret')
+    args.add_argument('--bbox', type=str, default='13.4,52.5,13.6,52.7', help='Bounding box in the format minx,miny,maxx,maxy')
+    args.add_argument('--time', type=str, default='2021-05/2021-08', help='Time range of the image in the format YYYY-MM/YYYY-MM')
+    args.add_argument('--maxcc', type=int, default=20, help='Maximum cloud cover percentage')
+    args.add_argument('--threshold', type=float, default=0.5, help='Threshold for inference')
+    args = args.parse_args()
+
+    # Store parsed arguments in global variables
+    INSTANCE_ID = args.instance_id
+    CLIENT_ID = args.client_id
+    CLIENT_SECRET = args.client_secret
+    BBOX = args.bbox
+    TIME = args.time
+    MAX_CC = args.maxcc
+    THRESHOLD = args.threshold
+
+    print(f'Command line arguments parsed successfully: {BBOX}, {TIME}, {MAX_CC}, {THRESHOLD}')
+
+    return args
+
+
+def main():
+    
+    setup_argparse()
+
+    data = request_data_sh(CLIENT_ID, CLIENT_SECRET, BBOX, TIME, MAX_CC)
+
+    # run the ship detector function on the downloaded data
+    ship_detector(data, THRESHOLD, min_size_threshold=10)
+    
+if __name__ == "__main__":
+    main()
---
+++ src/utils/utils.py
@@ -0,0 +1,514 @@
+import os
+import argparse
+import time
+import urllib
+from urllib.error import HTTPError
+import requests
+import time
+import logging
+import datetime
+import math
+import tifffile
+from PIL import Image
+
+from sentinelhub import (
+    BBox,
+    SentinelHubCatalog,
+    SentinelHubRequest,
+    SentinelHubDownloadClient,
+    Geometry,
+    SHConfig,
+    CRS,
+    DataCollection,
+    MimeType,
+)
+
+import numpy as np
+import tifffile
+from skimage import io as tiff
+import matplotlib.pyplot as plt
+import random
+import rasterio
+import netCDF4 as nc
+
+import tensorflow as tf
+from tensorflow.keras import models, layers, regularizers
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Conv2D, Activation, Add
+import cv2
+
+
+
+args = None
+INSTANCE_ID = None
+CLIENT_ID = None
+CLIENT_SECRET = None
+LAYER_NAME = None
+BBOX = None
+TIME= None
+MAX_CC = None
+THRESHOLD = None
+
+
+
+SERVER_URL = 'https://services.sentinel-hub.com/ogc/wms/'
+
+
+def request_data(SERVER_URL, INSTANCE_ID, BBOX, TIME, MAX_CC, max_retries=3, retry_delay=5):
+    '''
+    Function to query and request data from Sentinel Hub WMS based on parsed arguments
+    SERVER_URL: Sentinel Hub WMS server URL (fixed value)
+    INSTANCE_ID: Sentinel Hub instance ID (from parsed arguments)
+    BBOX: Bounding box in the format minx,miny,maxx,maxy (from parsed arguments)
+    TIME: Time range of the image in the format YYYY-MM-DD/YYYY-MM-DD (from parsed arguments)
+    MAX_CC: Maximum cloud cover percentage (from parsed arguments)
+    '''
+    max_retries = 3
+    retry_delay = 3
+
+    wms_query = f'{SERVER_URL}{INSTANCE_ID}?REQUEST=GetMap&CRS=CRS:84&BBOX={BBOX}&LAYERS=L2A-4-BAND&WIDTH=512&HEIGHT=343&FORMAT=image/tiff&TIME={TIME}&MAXCC={MAX_CC}'
+    
+    print(wms_query)
+    for i in range(max_retries):
+        try:
+            response = requests.get(wms_query)
+
+        except HTTPError as e:
+            print(f'HTTP error occurred: {e}')
+            if i < max_retries - 1:
+                print(f"Retrying in {retry_delay} seconds...")
+                time.sleep(retry_delay)
+            else:
+                raise Exception(f"Failed to request data from Sentinel Hub WMS after {max_retries} retries. Last error: {e}")
+
+        if response.status_code == 200:
+            print("Data request successful.")
+            print(response.headers)
+            return response.content
+        else:
+            if i < max_retries - 1:
+                print(f"Retrying in {retry_delay} seconds...")
+                time.sleep(retry_delay)
+            else:
+                raise Exception(f"Failed to request data from Sentinel Hub WMS after {max_retries} retries. Status code: {response.status_code}")
+            
+def request_data_sh(CLIENT_ID, CLIENT_SECRET,BBOX, TIME,MAX_CC):
+
+    #Set up the SH config
+    sh_client_id = CLIENT_ID
+    sh_client_secret = CLIENT_SECRET
+    
+    if not sh_client_id or not sh_client_secret:
+        sh_client_id = os.environ.get('SH_CLIENT_ID')
+
+    if not sh_client_id or not sh_client_secret:
+        sh_client_secret = os.environ.get('SH_CLIENT_SECRET')
+
+    if not sh_client_id or not sh_client_secret:
+        raise Exception("Please provide the credentials for Sentinel Hub.")
+    
+    config = SHConfig()
+    config.sh_client_id = sh_client_id
+    config.sh_client_secret = sh_client_secret
+    config.sh_base_url = 'https://services.sentinel-hub.com'
+
+    custom_evalscript = """
+    //VERSION=3
+
+    function setup() {
+        return {
+            input: [{
+                bands: ["B02", "B03", "B04", "B08"]
+            }],
+            output: {
+                bands: 4
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [sample.B04, sample.B03, sample.B02, sample.B08];
+    }
+    """
+
+    catalog = SentinelHubCatalog(config=config)
+ 
+    time_str = TIME
+    start_date_str, end_date_str = time_str.split("/")
+    time_interval = (start_date_str, end_date_str)
+
+    print(time_interval)
+    bbox=BBox(bbox=BBOX, crs=CRS.WGS84)
+  
+    search_iterator = catalog.search(
+        DataCollection.SENTINEL2_L2A,
+        bbox=bbox,
+        time=time_interval,
+        filter=f'eo:cloud_cover < {MAX_CC}',
+        fields= {"include": ["id","properties.datetime", "properties.cloudCover"], "exclude": []},
+    )
+
+
+    results = list(search_iterator)
+    print(f"Found {len(results)} results")
+
+    process_requests = []
+
+    for result in results:
+        request = SentinelHubRequest(
+            evalscript=custom_evalscript,
+            input_data=[
+                SentinelHubRequest.input_data(
+                    data_collection=DataCollection.SENTINEL2_L2A,
+                    time_interval=time_interval,
+                    mosaicking_order='leastCC'
+                )
+            ],
+            responses=[
+                SentinelHubRequest.output_response('default', MimeType.TIFF)
+            ],
+            bbox=bbox,
+            config=config
+        )
+
+        process_requests.append(request)
+
+        client = SentinelHubDownloadClient(config=config)
+        download_requests = [request.download_list[0] for request in process_requests]
+        data = client.download(download_requests, max_threads=3)
+
+        np.save('image.npy', data)
+
+        print("Data downloaded")
+
+        datacheck = np.load('image.npy')
+
+        blue = Image.fromarray(datacheck[0][:, :, 0])
+        green = Image.fromarray(datacheck[0][:, :, 1])
+        red = Image.fromarray(datacheck[0][:, :, 2])
+        nir = Image.fromarray(datacheck[0][:, :, 3])
+        rgb_image = np.stack((datacheck[0][:, :, 0], datacheck[0][:, :, 1], datacheck[0][:, :, 2]), axis=-1)
+        pil_image = Image.fromarray(rgb_image)
+
+
+        pil_image.save('image.tiff')
+        blue.save('blue.tiff')
+        green.save('green.tiff')
+        red.save('red.tiff')
+        nir.save('nir.tiff')
+
+        return data
+
+
+def calculate_ndwi(nir_band, green_band):
+    
+    nir_band_float = nir_band.astype(float)
+    green_band_float = green_band.astype(float)
+    
+    nir_band_float = np.nan_to_num(nir_band_float, nan=0.0)
+    green_band_float = np.nan_to_num(green_band_float, nan=0.0)
+    
+    # Replace zero values in the red band with a small positive value (1e-6)
+    nir_band_float[nir_band_float == 0] = 1e-6
+    green_band_float[green_band_float == 0] = 1e-6
+
+    
+    ndwi = (green_band_float - nir_band_float) / (green_band_float + nir_band_float)
+    return ndwi
+
+
+def inf_true_color_images(region_folder):
+    
+    print("True color process ongoing...")
+
+    red_band_path = None
+    green_band_path = None
+    blue_band_path = None
+
+    for file in os.listdir(region_folder):
+        if 'B04' in file:
+            red_band_path = os.path.join(region_folder, file)
+            print(f"B04 found, processing file: {file}")
+        elif 'B03' in file:
+            green_band_path = os.path.join(region_folder, file)
+            print(f"B03 found, processing file: {file}")
+        elif 'B02' in file:
+            blue_band_path = os.path.join(region_folder, file)
+            print(f"B02 found, processing file: {file}")
+
+    if red_band_path and green_band_path and blue_band_path:
+        with rasterio.open(red_band_path) as red_band_src, \
+            rasterio.open(green_band_path) as green_band_src, \
+            rasterio.open(blue_band_path) as blue_band_src:
+
+            red_band = red_band_src.read(1)
+            green_band = green_band_src.read(1)
+            blue_band = blue_band_src.read(1)
+
+            red_band_norm = red_band.astype(float) / np.iinfo(red_band.dtype).max
+            green_band_norm = green_band.astype(float) / np.iinfo(green_band.dtype).max
+            blue_band_norm = blue_band.astype(float) / np.iinfo(blue_band.dtype).max
+
+            true_color = np.dstack((red_band_norm, green_band_norm, blue_band_norm))
+
+    else:
+        print(f"Band files not found for region: {region_folder}")
+    return true_color 
+
+
+
+def true_color(red_band,green_band,blue_band):
+    
+    true_color = np.dstack((red_band, green_band, blue_band))
+    # Assuming you have a NumPy array called stored_rgb
+    normalized_stored_rgb = true_color.astype(float) / np.max(true_color)
+
+    # Scale to the [0, 255] range
+    scaled_stored_rgb = (normalized_stored_rgb * 255).astype(np.uint8)
+
+        # Assuming you have a NumPy array called stored_rgb with integer values in [0, 255] range
+    scaled_stored_rgb = scaled_stored_rgb.astype(float)  # Convert to float for accurate multiplication
+    
+    luminance_factor = 4.0 # Adjust this value to control the luminance
+    
+    # Scale the pixel values to increase luminance
+    brightened_image = scaled_stored_rgb * luminance_factor
+    
+    # Clip the values to ensure they remain within the valid [0, 255] range
+    brightened_image = np.clip(brightened_image, 0.0, 255.0)
+    
+    # Convert the image back to the integer data type (uint8)
+    brightened_image = brightened_image.astype(np.uint8)
+        
+    brightened_image = np.array(brightened_image)
+
+    return brightened_image 
+
+
+def spatiotemp_img(blue_band, red_band):
+    # Convert input bands to float and handle invalid values
+    blue_band_float = blue_band.astype(float)
+    red_band_float = red_band.astype(float)
+
+    # Replace NaN and infinity values with zero
+    blue_band_float = np.nan_to_num(blue_band_float, nan=0.0)
+    red_band_float = np.nan_to_num(red_band_float, nan=0.0)
+    
+    # Replace zero values in the red band with a small positive value (1e-6)
+    red_band_float[red_band_float == 0] = 1e-6
+
+    img = blue_band_float / (red_band_float+blue_band_float)
+    
+    return img
+
+
+
+
+def plot_dataset(dataset, band_names):
+    num_bands = len(band_names)
+    
+    plt.figure(figsize=(18, 6))
+    
+    region_data = dataset[0]  # Assuming there's only one region
+    
+    for j in range(num_bands):
+        plt.subplot(1, num_bands, j + 1)
+        plt.imshow(region_data[j], cmap='gray')
+        plt.title(f"Band {band_names[j]}")
+    
+    plt.tight_layout()
+    plt.show()
+
+
+
+def inference_tiles(dataset, tile_size):
+    # Convert the list of band images to a NumPy array
+    band_images = np.array(dataset)
+
+    # Split the dataset into tiles
+    tiled_dataset = []
+    
+    for band_image in band_images:
+        if band_image.shape[0] >= 1:
+            band_image = np.transpose(band_image, (1, 2, 0))
+        
+        num_tiles_x = band_image.shape[1] // tile_size
+        num_tiles_y = band_image.shape[0] // tile_size
+        
+        for tile_y in range(num_tiles_y):
+            for tile_x in range(num_tiles_x):
+                start_x = tile_x * tile_size
+                start_y = tile_y * tile_size
+
+                band_tile = band_image[start_y:start_y + tile_size, start_x:start_x + tile_size]
+                tiled_dataset.append(band_tile)
+        updated_shape = num_tiles_y*tile_size , num_tiles_x*tile_size
+    
+    print("Raw shape of the dataset was", band_image.shape)
+    print("New shape is", updated_shape)
+    return tiled_dataset, updated_shape
+ 
+
+def plot_tiled_dataset(tiled_dataset):
+    num_patches_in_pair = tiled_dataset[0].shape[2]  # Assuming all arrays in the list have the same shape
+
+    # Randomly select an index from the tiled_dataset
+    index = random.randint(0, len(tiled_dataset) - 1)
+
+    plt.figure(figsize=(18, 24))
+
+    for band in range(num_patches_in_pair):
+        plt.subplot(num_patches_in_pair, num_patches_in_pair, num_patches_in_pair + band + 1)
+        plt.imshow(tiled_dataset[index][:, :, band], cmap='gray')  # Using grayscale colormap
+        plt.title(f"Patch {index + 1}, Band {band + 1}")
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()            
+
+
+            
+def ship_detector(data, threshold, min_size_threshold=10):
+    
+    blue = None
+    green = None
+    red = None
+    nir = None
+
+    blue = data[0][:, :, 2]
+    green = data[0][:, :, 1]
+    red = data[0][:, :, 0]
+    nir = data[0][:, :, 3]
+
+    blue_max = np.max(blue)
+    blue = blue / blue_max
+    
+    green_max = np.max(green)
+    green = green / green_max
+    
+    red_max = np.max(red)
+    red = red / red_max
+    
+    nir_max = np.max(nir)
+    nir = nir / nir_max
+
+    stored_NDWI = calculate_ndwi(nir,green)
+    stored_spatio_temp = spatiotemp_img(blue,red)
+    stored_rgb =  true_color(red,green,blue)
+
+    bands = [red, green, blue, stored_NDWI, nir, stored_spatio_temp]
+    
+        # Determine the shape of the first band
+    target_shape = bands[0].shape[:2]
+    
+        # Reshape each band to the target shape
+    bands = [band.reshape(target_shape) for band in bands]
+           
+    dataset = []
+    dataset.append(tuple(bands))
+    
+    patches, updated_shape = inference_tiles(dataset, tile_size= 64)
+
+        # Provide the path to the model file
+    model_path = './draft_model/Multihead_Attention_UNet_model.h5'
+    model = tf.keras.models.load_model(model_path, custom_objects={"K":K}, compile=False)
+
+    binary_masks = []
+
+    for idx, patch in enumerate(patches):
+        test_img = patch
+
+        # Expand dimensions of the test image and make predictions
+        test_img_input = np.expand_dims(test_img, 0)
+        prediction = (model.predict(test_img_input)[0, :, :, 0] > threshold).astype(np.uint8)
+
+        # Append the binary mask to the list
+        binary_masks.append(prediction)
+        
+        #Print a consolidated progress line
+        print(f'{idx + 1}/{len(patches)} [===========================]', end='\r')
+
+    #Print a newline character to separate the progress line from other output
+    print()
+    
+    tile_size = 64 
+     
+    large_binary_mask = np.zeros(updated_shape, dtype=np.uint8)
+
+    num_tiles_x = updated_shape[1] // tile_size
+
+    # Loop through the binary masks and populate the large binary mask
+    for idx, mask_tile in enumerate(binary_masks):
+        # Calculate the tile's position in the reassembled binary mask
+        tile_y = idx // num_tiles_x  # Calculate tile index in y direction
+        tile_x = idx % num_tiles_x   # Calculate tile index in x direction
+
+        start_x = tile_x * tile_size
+        start_y = tile_y * tile_size
+
+        end_x = start_x + tile_size
+        end_y = start_y + tile_size
+
+        # Place the mask tile in the corresponding position
+        large_binary_mask[start_y:end_y, start_x:end_x] = mask_tile
+
+    
+    target_height, target_width = large_binary_mask.shape[:2]
+
+    # Crop stored_rgb to match the target size
+    cropped_rgb = stored_rgb[:target_height, :target_width, :]
+    
+    # Create a copy of the resized RGB image
+    overlay_rgb = np.copy(cropped_rgb)
+
+    # Apply the binary mask to the copy, making ship pixels red
+    overlay_rgb[large_binary_mask == 1, :] = [255, 0, 0]  # Red color for ship pixels
+
+       # Find connected components and label them
+    _, labeled_mask = cv2.connectedComponents(large_binary_mask)
+    
+    # Filter out small components (adjust min_size_threshold as needed)
+
+    unique_labels, label_counts = np.unique(labeled_mask, return_counts=True)
+    for label in unique_labels:
+        if label_counts[label] < min_size_threshold:
+            labeled_mask[labeled_mask == label] = 0
+    
+    # Find bounding boxes for the labeled vessels
+    bounding_boxes = []
+    for label in range(1, labeled_mask.max() + 1):  # Skip label 0 (background)
+        vessel_mask = (labeled_mask == label).astype(np.uint8)
+        contours, _ = cv2.findContours(vessel_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            x, y, w, h = cv2.boundingRect(contours[0])
+            bounding_boxes.append((x, y, x + w, y + h))
+    
+    # Draw bounding boxes on the overlayed RGB image
+    for x1, y1, x2, y2 in bounding_boxes:
+        cv2.rectangle(overlay_rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Draw green bounding boxes
+    
+    # Count the number of bounding boxes (vessel entities)
+    num_vessels = len(bounding_boxes)
+    
+    # Plot the overlayed RGB image with labeled vessels and bounding boxes
+    plt.figure(figsize=(10, 10))
+    plt.imshow(overlay_rgb)  # Convert BGR to RGB for displaying with matplotlib
+    plt.title(f'RGB Image with {num_vessels} Vessel Entities and Bounding Boxes')
+    plt.axis('off')
+    plt.show()
+    
+    print(f'Number of Vessel Entities: {num_vessels}')
+           
+
+           
+            
+                        
+            
+            
+            
+            
+            
+            
+            
+            
+            
